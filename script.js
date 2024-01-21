b94db30140894193191c3e92951a501136f4c00d
Jordan Stewart <81133588+JSordan22@users.noreply.github.com>
2024-01-21 16:42:56 -0600

    added game() and optimized playRound()

--- script.js
+++ script.js
@@ -6,21 +6,31 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
+    const outcomes = {"rock": {win: "scissors", lose: "paper"},
+                      "paper": {win: "rock", lose: "scissors"},
+                      "scissors": {win: "paper", lose: "rock"}}
 
-    if(playerSelection == "rock" && computerSelection == "rock" ||
-       playerSelection == "paper" && computerSelection == "paper" ||
-       playerSelection == "scissors" && computerSelection == "scissors") {
+    if(playerSelection == computerSelection) {
         console.log("You Tied! " + playerSelection + " ties with " + computerSelection);
-        playRound(prompt("Pick rock, paper, or scissors"), getComputerChoice());
-    } else if(playerSelection == "rock" && computerSelection == "scissors" ||
-              playerSelection == "scissors" && computerSelection == "paper" ||
-              playerSelection == "paper" && computerSelection == "rock") {
+        playRound(prompt("Pick rock, paper, or scissors."), getComputerChoice());
+    } else if(outcomes[playerSelection].win == computerSelection) {
                 console.log("You Win! " + playerSelection + " beats " + computerSelection);
-    } else if(playerSelection == "rock" && computerSelection == "paper" ||
-              playerSelection == "paper" && computerSelection == "scissors" ||
-              playerSelection == "scissors" && computerSelection == "rock") {
+    } else if(outcomes[playerSelection].lose == computerSelection) {
                 console.log("You Lose! " + playerSelection + " loses to " + computerSelection);
+    } else {
+        console.log("Choose either rock, paper, or scissors.");
+        playRound(prompt("Pick rock, paper, or scissors"), getComputerChoice());
     }
 }
 
-playRound(prompt("Pick rock, paper, or scissors"), getComputerChoice());
+//playRound(prompt("Pick rock, paper, or scissors"), getComputerChoice());
+
+function game() {
+    playRound(prompt("Pick rock, paper, or scissors"), getComputerChoice());
+    playRound(prompt("Pick rock, paper, or scissors"), getComputerChoice());
+    playRound(prompt("Pick rock, paper, or scissors"), getComputerChoice());
+    playRound(prompt("Pick rock, paper, or scissors"), getComputerChoice());
+    playRound(prompt("Pick rock, paper, or scissors"), getComputerChoice());
+}
+
+game();

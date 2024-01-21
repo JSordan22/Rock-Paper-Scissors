5e9a364a86efe64bf4850cc5db82b43806009299
Jordan Stewart <81133588+JSordan22@users.noreply.github.com>
2024-01-21 17:34:01 -0600

    added round functionality to game() and fixed playRound()

--- script.js
+++ script.js
@@ -12,25 +12,31 @@ function playRound(playerSelection, computerSelection) {
 
     if(playerSelection == computerSelection) {
         console.log("You Tied! " + playerSelection + " ties with " + computerSelection);
-        playRound(prompt("Pick rock, paper, or scissors."), getComputerChoice());
+        return playRound(prompt("Pick rock, paper, or scissors"), getComputerChoice());
     } else if(outcomes[playerSelection].win == computerSelection) {
-                console.log("You Win! " + playerSelection + " beats " + computerSelection);
+        console.log("You Win! " + playerSelection + " beats " + computerSelection);
+        return 1;
     } else if(outcomes[playerSelection].lose == computerSelection) {
-                console.log("You Lose! " + playerSelection + " loses to " + computerSelection);
-    } else {
-        console.log("Choose either rock, paper, or scissors.");
-        playRound(prompt("Pick rock, paper, or scissors"), getComputerChoice());
+        console.log("You Lose! " + playerSelection + " loses to " + computerSelection);
+        return 0;
     }
 }
 
 //playRound(prompt("Pick rock, paper, or scissors"), getComputerChoice());
 
-function game() {
-    playRound(prompt("Pick rock, paper, or scissors"), getComputerChoice());
-    playRound(prompt("Pick rock, paper, or scissors"), getComputerChoice());
-    playRound(prompt("Pick rock, paper, or scissors"), getComputerChoice());
-    playRound(prompt("Pick rock, paper, or scissors"), getComputerChoice());
-    playRound(prompt("Pick rock, paper, or scissors"), getComputerChoice());
+function game(rounds) {
+    let wins = 0;
+
+    for(let i = 0; i < rounds; i++) {
+        wins += playRound(prompt("Pick rock, paper, or scissors"), getComputerChoice());
+    }
+
+    if(wins >= Math.ceil(rounds/2)) {
+        console.log("You won! you won " + wins + " rounds");
+    } else {
+        console.log("You lost! you lost " + (rounds-wins) + " rounds");
+    }
+    console.log(wins)
 }
 
-game();
+game(5);
